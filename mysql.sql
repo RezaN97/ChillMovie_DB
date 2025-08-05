ccbe354b1a313a04d811f859d68fc9244d944b94
RezaN97 <rezanurit16@gmail.com>
2025-08-06 00:17:04 +0700

    update sql file

--- mysql.sql
+++ mysql.sql
@@ -8,11 +8,3 @@ CREATE TABLE movie (
     genre VARCHAR(100) NOT NULL,
     created TIMESTAMP NOT NULL DEFAULT NOW()
 );
-<<<<<<< HEAD
-=======
-
-INSERT INTO movie (title, description, genre)
-VALUES
-('Moon Knight','Menceritakan Hero Berasal dari daerah Mesir','Action'),
-('Avangers','Menceritakan tentang sekelompok super hero yang bertugas menjaga keamanan di bumi','Action, Sci-FI');
->>>>>>> bf76c5fbfd94e81f9aa406d07eddcceecef58066
